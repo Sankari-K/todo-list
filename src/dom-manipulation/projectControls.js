0da96aadb7a99dbe180b0b97cdc8ccbe3982568a
Sankari Karthik <sankarikarthik03@gmail.com>
2023-03-22 15:27:28 +0530

    Add functionality to delete projects

--- src/dom-manipulation/projectControls.js
+++ src/dom-manipulation/projectControls.js
@@ -1,6 +1,21 @@
 import { Projects } from "../modules/projects";
 
 
+const deleteProject = (e) => {
+    Projects.deleteProject(e.target.id);
+    refreshProject();
+}
+
+const refreshProject = () => {
+    let projects = document.querySelector('#projects').children;
+    for (let project of projects) {
+        let name = project.querySelector('span').textContent;
+        if (!(name in Projects.projectList)) {
+            project.remove();
+        }
+    }
+}
+
 // this is called when someone clicks on the "Add Project" button
 const addProject = () => {
     let addProjectElement = document.querySelector('#add-project');
@@ -33,21 +48,35 @@ const addProject = () => {
             exitControls();
             return;
         }
+        else if (content.value in Projects.projectList) {
+            window.alert("Project already exists");
+            exitControls();
+            return;
+        }
 
         let newProjectName = content.value;
         // this is where we have to add a new project
         let projectContainer = document.querySelector('#projects');
 
         let element = document.createElement('div');
+
         let image = document.createElement('img');
         image.src = "../src/assets/side-nav/project.png";
         image.alt = "project icon";
+
+        let cancel = document.createElement('img');
+        cancel.src = "../src/assets/side-nav/cancel.png";
+        cancel.alt = "delete project";
+        cancel.setAttribute('class', 'cancel');
+        cancel.setAttribute('id', newProjectName);
+        cancel.addEventListener('click', (e) => deleteProject(e));
 
         let name = document.createElement('span');
         name.textContent = newProjectName;
         element.append(
             image,
-            name
+            name,
+            cancel
         )
         projectContainer.appendChild(element)
         content.value = "";
@@ -58,5 +87,7 @@ const addProject = () => {
 };
 
 export {
-    addProject
+    addProject,
+    deleteProject,
+    refreshProject
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './style.css';
-import { addProject } from './dom-manipulation/add-project';
+import { addProject } from './dom-manipulation/projectControls';
 import { Projects } from './modules/projects';
 
 document.querySelector('#add-project').addEventListener('click', addProject);
 
+// document.querySelector('#add-project').addEventListener('click', () => {
+//     console.log("woot!");
+//     console.log(Projects)
+// });
 
-document.querySelector('#add-project').addEventListener('click', () => {
-    console.log("woot!");
-    console.log(Projects)
-});
 
