be463d7caf7f182e5998d738fae9547c51ac3361
Sankari Karthik <sankarikarthik03@gmail.com>
2023-03-26 18:54:25 +0530

    Fix consistency when todo is deleted in the inbox

--- src/dom-manipulation/deleteToDo.js
+++ src/dom-manipulation/deleteToDo.js
@@ -2,28 +2,26 @@ import { Projects } from "../modules/projects";
 import { exitEditForm } from "./editToDo";
 
 const deleteToDo = (e) => {
-    let projectName = document.querySelector('.content').querySelector('span').textContent;
     let toDelete = e.currentTarget.correspondingToDo;
+    let projectName = toDelete.project;
 
     // if the edit option was opened for this, close that
     let todoDOM = e.currentTarget.parentNode.parentNode;
-    console.log(todoDOM);
     if (todoDOM.nextSibling != null && todoDOM.nextSibling.id == "new-todo-edit") {
         exitEditForm();
     }
 
     let DOMContainer = document.querySelector('.todos').children;
-    let index = 0;
     for (const todo of DOMContainer) {
         let deleteElement = todo.querySelector('.edits').lastChild.correspondingToDo;
-        if (deleteElement == toDelete) {
+        if (deleteElement == toDelete && toDelete.project == deleteElement.project) {
             // remove it from the DOM
             todo.remove();
             // now, delete it from the data structure
+            let index = Projects.projectList[projectName].todos.indexOf(toDelete);
             delete Projects.projectList[projectName].todos[index]
             Projects.projectList[projectName].todos.splice(index, 1);
         };
-        index += 1;
     }
 }
 
